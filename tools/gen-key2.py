46ee28ed58a6e8dfb9eb0d62f38cb2e3b706b2f2
johnsond1 <david.johnson@binarydefense.com>
2020-01-27 10:23:17 -0500

    :pencil: Add comments
    I added comments documenting the issues I am currently encountering.

--- tools/gen-key2.py
+++ tools/gen-key2.py
@@ -9,6 +9,11 @@ with open(path, "r") as f:
 ca = crypto.load_certificate(crypto.FILETYPE_PEM, cert)
 ca_key = ca.get_pubkey()
 pub_key = crypto.dump_publickey(crypto.FILETYPE_PEM, ca_key)
-
 print(pub_key)
+
+# so far this matches main.rb
+# the issue that I am encountering is the private key and group
+# https://www.pyopenssl.org/en/stable/api/crypto.html#pkey-objects
+# https://ruby.github.io/openssl/OpenSSL/PKey/EC.html#method-i-public_key
+
 pubkey = b"1aac545aa9f96823e77ad5246f53c65ad84babc6d5b6d1e67371aedd9cd60c61fddba08903b80514ec57ceee5d3fe221b3cef7d48a79e0a3837e2d97d061c4f199dc259163ab7f30a3b470e2c7a1339cf3bf2e5c53b15fb37d327f8a34e37979"
